ce35a8cb7b16e4e8bba9a94235080a98769dcce0
Nicole Giron <70114716+nicolegiron@users.noreply.github.com>
2021-11-02 21:59:15 -0400

    Add files via upload

---
+++ starter2.4.py
@@ -0,0 +1,11 @@
+# Recitation Activity #2
+# You must upload and submit your file before the end of your recitation
+#
+# General Instructions
+#  - Do NOT change the function name
+#  - Read the four questions presented in recitation
+#  - Change "ans1", "ans2", "ans3", "ans4" with your answers
+
+
+def answers():
+    return "746ps", "c", "c", "e"
